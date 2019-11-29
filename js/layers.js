d2444a3dbb4d16e30d4b812b2b9f1e156efcfc37
AutumnSun <q19960105@163.com>
2019-11-29 11:15:12 +0800

    update style

--- js/layers.js
+++ js/layers.js
@@ -1,22 +1,5 @@
-let conf = {
-  "flow-icon": "triangle-red",
-  "camera-icon": "circle-blue",
-  "poi-fill": {
-    'fill-opacity': 0.3,
-    'fill-pattern': "fill-line0011",
-  },
-  "fn-fill": {
-    'fill-opacity': 0.3,
-    'fill-pattern': "fill-line1000",
-  },
-}
-filter = ['all',
-  ['match', ['get', 'Layer'], ['P-平面到顶固定家具', 'P-平面洁具', '厨房设备', 'P-装饰完成面到顶', 'F-地坪文本'], false, true],
-  ['match', ['get', 'EntityHandle'], ["4336"], false, true],
-]
 let layers = [
   {
-    //整体地图背景
     'id': 'background',
     'type': 'background',
     'paint': {
@@ -28,8 +11,8 @@ let layers = [
     'type': 'fill',
     'source': 'Source',
     'paint': {
-      'fill-opacity': 0.5,
-      'fill-color': '#678678'
+      'fill-opacity': 0.3,
+      'fill-pattern': "fill-line1000",
     },
     'filter': ["match", ["geometry-type"], ["Polygon", "MultiPolygon"], true, false]
   },
@@ -48,11 +31,7 @@ let layers = [
     'source': 'Source',
     'paint': {
       'circle-color': '#880033',
-      'circle-radius': 10
+      'circle-radius': 5
     },
-    'filter': ['all',
-      ['match', ['get', 'Layer'], ['楼层文字',], true, false],
-      ["match", ["geometry-type"], ["Point"], true, false],
-    ]
   },
 ]
